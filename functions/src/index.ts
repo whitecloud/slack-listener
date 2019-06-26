00f3e6427c9d1f56eeb6aa230ef8b29c903812ed
Matttaylor8910 <taylor.matt777@gmail.com>
2019-06-26 14:58:32 -0600

    Add our custom responses

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,45 @@ const ignoreSlackBots = true;
 
 // list of responses that we could send back
 const responses = [
-  'Cat poop sleeps for no one',
+  'I feel like crap on a cracker',
+  'I lied about the coming in part',
+  'Tube-feeding a cat every four hours',
+  'Hmmm, I seem to have developed a wicked cough',
+  'I’m running on fumes, gonna go home for a nap',
+  'Gonna go pick up one kitty from the vet',
+  'Geez, happening again… all of a sudden I’m massively nodding off. Gonna drive home while I can keep my eyes open',
+  'I have a vet appointment',
+  'Need to do some car stuff, and have a goofy webinar tomorrow morning',
+  'No hair emergency, but a cat emergency',
+  'Sudden hair emergency',
+  'Been fighting a bad headache all day, and I’m losing the battle',
+  'Hells bells... more driveway duty',
+  'I’m having one of those days where I’m doing more harm than good',
+  'So, this is ridiculous…  I’m falling asleep at my desk',
+  'I’m in my bunny slippers and I just don’t want to put on shoes',
+  'Yet another interaction with the medical community',
+  'Pretty whammin’ headache plus the hot-and-colds',
+  'Sigh, double emergency (cat plus electrical)',
+  'MIGHT have to leave suddenly',
+  'Still sorta falling asleep at my desk, plus Justin had to explain to me why it looks like a person burning on the FunSchway tv screen. Anyway, I don’t think work is in the cards for me right now',
+  'This is ridiculous. I literally cannot stay awake',
+  'Lotta nitrous in the last few days. There’s got to be an easier way to get high',
+  'This is WAY past getting old...  I fell again today',
+  '😠 <insert some choice swear words in here>  I just slipped on my newly-waxed (not by me) wood floor, and completely screwed up my hip again. Can’t sit, can’t stand, and sure as HELL can’t do stairs',
+  'I need drugs, which I unfortunately left at home',
+  'Falling asleep at my desk',
+  'Gotta get a haircut',
+  'Having trouble with my eyes, and I’ve got goop at home that will help',
+  'This is ridiculous — I’m literally falling asleep at my desk',
+  'Dueling sound systems is making my head throb',
+  'Cat poop sleeps for no one'
 ];
 
 // list of trigger words (case insensitive)
 const triggerWords = [
-  'WFH'
+  'WFH',
+  'Work from home',
+  'I will be in afterwards'
 ];
 
 /**
